1dcb9b8e84c4baf4f6038b11650fce0e219fca57
Khavkin <hdm@ukr.net>
2025-02-17 20:38:19 +0200

    added app screens

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+export default function HomeScreen() { 
+return (<View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+<Text>Edit app/(tabs)/index.tsx to edit this screen.</Text>
+    </View>)
+}
---
+++ app/(tabs)/options/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from "expo-router";
+
+
+export default function OptionsTabsLayout() { 
+    return (
+        <Tabs>
+            <Tabs.Screen 
+            name="index"
+                    options={{
+                      title: 'Options',
+                     // tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+                    }}
+            />
+            <Tabs.Screen
+                    name="profile"
+                    options={{
+                      title: 'Profile',
+                     // tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+                    }}
+                  />
+        </Tabs>
+    )
+}
